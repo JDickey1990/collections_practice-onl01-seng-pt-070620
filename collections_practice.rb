54731265ec3d7cd881547779eaa53b5875babba8
Jordan Dickey <jordan.michael.dickey@gmail.com>
2020-07-14 21:49:23 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -26,7 +26,12 @@ def sort_array_char_count (array)
 end
 end
 
-
 def swap_elements (array)
 array[0], array[1], array[2] = array[0], array[2], array[1]
 end
+
+def reverse_array(array)
+  array.reverse
+end
+  
+  
