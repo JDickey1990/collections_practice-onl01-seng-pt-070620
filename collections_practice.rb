3a5af34c4488157ca60266d76f87956e0969813d
Jordan Dickey <jordan.michael.dickey@gmail.com>
2020-07-14 21:39:17 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -15,7 +15,14 @@ end
 end
 
 def sort_array_char_count (array)
-
+if a.length == b.length
+    0
+  elsif a.length < b.length
+    1
+  elsif a.length > b.length
+    -1
+  end
+end
 end
 
 def swap_elements (array)
